f2d49bf2eecc39986e0663fc1b592e4e1bcfb8ef
TechAsish1212 <gitasish1212@gmail.com>
2026-04-29 23:40:13 +0530

    feat:learn about duck or structural typing

--- Object/src/objectTs.ts
+++ Object/src/objectTs.ts
@@ -31,3 +31,35 @@ const adarkChai:Tea={
     price:20,
     ingredients:["Ginger","tea leaves"]
 }
+
+// Structural Typing or duck typing
+// Type compatibility is based on the structure (shape) of the object, not the type name.
+// If an object has the required properties, it can be assigned.
+
+type Cup={
+    size:string;
+}
+
+let smallCup:Cup={
+    size:"200ml",
+}
+
+let bigCup={
+    size:"500ml",
+    material:"steel",
+}
+
+smallCup=bigCup;
+
+
+// another ex;
+type Brew={
+    brewType:number;
+}
+
+const cofee={
+    brewType:5,
+    beans:"Arabic"
+}
+
+const chaiBrew:Brew=cofee
