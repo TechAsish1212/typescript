cb11696eaa95a33e080769e2ab963e091f45f9c6
TechAsish1212 <gitasish1212@gmail.com>
2026-04-30 00:13:41 +0530

    feat:learn about Pick and Omit

--- Object/src/objectTs.ts
+++ Object/src/objectTs.ts
@@ -1,97 +1,142 @@
-const chai={
-    name:"masala chai",
-    price:20,
-    isHot:true
+const chai = {
+    name: "masala chai",
+    price: 20,
+    isHot: true
 }
 
 // declaring object type
-let tea:{
-    name:string;
-    price:number;
-    isHot:true;
+let tea: {
+    name: string;
+    price: number;
+    isHot: true;
 }
 
-tea ={
-    name:"Masala chai",
-    price:20,
-    isHot:true
+tea = {
+    name: "Masala chai",
+    price: 20,
+    isHot: true
 }
 
 // alias object
 // A type alias lets you give a name to an object structure.
-type Tea={
-    name:string;
-    price:number;
-    ingredients:string[],
+type Tea = {
+    name: string;
+    price: number;
+    ingredients: string[],
 }
 
 // Using the alias
-const adarkChai:Tea={
-    name:"Adrak chai",
-    price:20,
-    ingredients:["Ginger","tea leaves"]
+const adarkChai: Tea = {
+    name: "Adrak chai",
+    price: 20,
+    ingredients: ["Ginger", "tea leaves"]
 }
 
 // Structural Typing or duck typing
 // Type compatibility is based on the structure (shape) of the object, not the type name.
 // If an object has the required properties, it can be assigned.
 
-type Cup={
-    size:string;
+type Cup = {
+    size: string;
 }
 
-let smallCup:Cup={
-    size:"200ml",
+let smallCup: Cup = {
+    size: "200ml",
 }
 
-let bigCup={
-    size:"500ml",
-    material:"steel",
+let bigCup = {
+    size: "500ml",
+    material: "steel",
 }
 
-smallCup=bigCup;
+smallCup = bigCup;
 
 
 // another ex;
-type Brew={
-    brewType:number;
+type Brew = {
+    brewType: number;
 }
 
-const cofee={
-    brewType:5,
-    beans:"Arabic"
+const cofee = {
+    brewType: 5,
+    beans: "Arabic"
 }
 
-const chaiBrew:Brew=cofee
+const chaiBrew: Brew = cofee
 
 // type composition
 // This is Type Composition in TypeScript, where complex types are built using smaller reusable custom types
-type Item={
-    name:string;
-    quantity:number;
+type Item = {
+    name: string;
+    quantity: number;
 }
 
-type Address={
-    street:string;
-    pin:number;
+type Address = {
+    street: string;
+    pin: number;
 }
 
-type Order={
-    id:string;
-    items:Item[];
-    addresses:Address
+type Order = {
+    id: string;
+    items: Item[];
+    addresses: Address
 }
 
 
 // Partial<T> is a built-in TypeScript utility type that makes all properties of a type optional.
-type Chai={
-    name:string;
-    price:number;
-    isHot:boolean;
+type Chai = {
+    name: string;
+    price: number;
+    isHot: boolean;
 }
 
-const updateChai=(updates:Partial<Chai>)=>{ // Partial<Chai>--->It converts all properties of Chai into optional properties.
-    console.log("updating chai with:: ",updates);
+const updateChai = (updates: Partial<Chai>) => { // Partial<Chai>--->It converts all properties of Chai into optional properties.
+    console.log("updating chai with:: ", updates);
 }
 
-updateChai({price:30})
+updateChai({ price: 30 })
+
+
+// Required<T> is a TypeScript utility type that makes all optional properties of a type mandatory.
+
+type ChaiOrder = {
+    name?: string;
+    quantity?: number;
+}
+
+const placeOrder = (order: Required<ChaiOrder>) => {
+    console.log(order);
+}
+
+placeOrder({
+    name: "Masala Chai",
+    quantity: 2
+})
+
+// Pick<T, K> is a TypeScript utility type that creates a new type by selecting specific properties from an existing type.
+
+type Chaika = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    ingredients: string;
+}
+
+type BasicChaiInfo = Pick<Chaika, "name" | "price">; // Take only name and price from Chaika
+
+const chaiInfo: BasicChaiInfo = {
+    name: "Lemon tea",
+    price: 25
+}
+
+// Omit<T, K> is a TypeScript utility type that constructs a new type by removing specified keys from an existing type.
+
+type ChaiNew = {
+    name: string;
+    price: number;
+    isHot: boolean;
+    secretIngredients: string;
+}
+
+type publicChai = Omit<ChaiNew, "secretIngredients">; // Remove secretIngredients from ChaiNew
+
