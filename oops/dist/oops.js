ec23c3b396e0a59a0e3856ed82583dfb4c662d4e
TechAsish1212 <gitasish1212@gmail.com>
2026-05-07 22:48:59 +0530

    feat: learn about Access Modifier in oop

--- oops/dist/oops.js
+++ oops/dist/oops.js
@@ -14,4 +14,38 @@ class person {
 }
 const p1 = new person("Asish", 21);
 p1.greet();
+// Access Modifiers
+// TypeScript adds access control:
+// public (default) → accessible everywhere
+// private → only inside the class
+// protected → inside class + subclasses
+class Chai {
+    flavour = 'Masala';
+    secretIngredients = "Cardamom";
+    reveal() {
+        console.log(this.secretIngredients);
+    }
+}
+// protected
+class Shop {
+    shopName = 'Chai Corner';
+}
+class Branch extends Shop {
+    getName() {
+        console.log(this.shopName);
+    }
+}
+// private
+class Walet {
+    #balance = 100;
+    getBalance() {
+        console.log(this.#balance);
+    }
+}
+const c = new Chai();
+c.reveal();
+const b = new Branch();
+b.getName();
+const w = new Walet();
+w.getBalance();
 //# sourceMappingURL=oops.js.map
--- oops/src/oops.ts
+++ oops/src/oops.ts
@@ -19,3 +19,66 @@ p1.greet();
 
 
 
+// Access Modifiers
+// TypeScript adds access control:
+// public (default) → accessible everywhere
+// private → only inside the class
+// protected → inside class + subclasses
+
+class Chai{
+    public flavour:string='Masala';
+
+    private secretIngredients="Cardamom";
+
+    reveal(){
+        console.log(this.secretIngredients);
+    }
+
+}
+
+// protected
+class Shop{
+    protected shopName:string='Chai Corner';
+
+}
+
+class Branch extends Shop{
+    getName(){
+        console.log(this.shopName);
+    }
+}
+
+// private
+class Walet{
+    #balance =100;
+
+    getBalance(){
+        console.log(this.#balance);
+    }
+}
+
+const c=new Chai();
+c.reveal();
+
+const b=new Branch();
+b.getName()
+
+const w=new Walet();
+w.getBalance();
+
+// getter and setter
+class ModernChai{
+    private _sugar=2;
+
+    get sugar(){
+        return this._sugar;
+    }
+
+    set sugar(value:number){
+        if(value>5) throw new Error ("Too sweet");
+        this._sugar=value;
+    }
+}
+
+const m=new ModernChai();
+m.sugar=4;
